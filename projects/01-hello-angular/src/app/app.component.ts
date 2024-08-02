f15e02c783acd207c5ba2963e12c7fc606ab36cc
LeventHAN <l.mehmed@faktion.com>
2024-08-02 19:58:36 +0200

    feat: finish p1

--- projects/01-hello-angular/src/app/app.component.ts
+++ projects/01-hello-angular/src/app/app.component.ts
@@ -9,7 +9,16 @@ import { RouterOutlet } from '@angular/router';
   template: `
     <h1>If you are reading this...</h1>
     <p>Things have worked out well! 🎉</p>
+    <ol>
+      <li>Song Song</li>
+      <li>Song Song 2</li>
+      <li>Song Song 3</li>
+    </ol>
   `,
-  styles: ``,
+  styles: `
+  ol {
+    list-style-type: upper-roman;
+  }
+  `,
 })
 export class AppComponent {}

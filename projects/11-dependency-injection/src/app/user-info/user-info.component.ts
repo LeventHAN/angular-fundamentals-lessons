dd47bf177785ef2307ea995f923578d68231a269
LeventHAN <l.mehmed@faktion.com>
2024-08-02 20:00:17 +0200

    feat: finish p11

--- projects/11-dependency-injection/src/app/user-info/user-info.component.ts
+++ projects/11-dependency-injection/src/app/user-info/user-info.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { User } from '../data';
 
 @Component({
@@ -11,28 +11,11 @@ import { User } from '../data';
   styles: ``,
 })
 export class UserInfoComponent {
-  // Test user data
-  user: User = {
-    id: -1,
-    name: 'Ervin Howell',
-    username: 'Antonette',
-    email: 'Shanna@melissa.tv',
-    address: {
-      street: 'Victor Plains',
-      suite: 'Suite 879',
-      city: 'Wisokyburgh',
-      zipcode: '90566-7771',
-      geo: {
-        lat: '-43.9509',
-        lng: '-34.4618',
-      },
-    },
-    phone: '010-692-6593 x09125',
-    website: 'anastasia.net',
-    company: {
-      name: 'Deckow-Crist',
-      catchPhrase: 'Proactive didactic contingency',
-      bs: 'synergize scalable supply-chains',
-    },
-  };
+  @Input({
+    required: true,
+  })
+  user!: User;
+
+
+
 }
--- projects/11-dependency-injection/src/app/user.service.ts
+++ projects/11-dependency-injection/src/app/user.service.ts
@@ -1,13 +1,21 @@
-import { data, User } from './data';
+import { Injectable } from '@angular/core';
+import { User } from './data';
 
+@Injectable({
+  providedIn: 'root',
+})
 export class UserService {
-  private userData: User[] = data;
-
   constructor() {}
 
   getUserData(): Promise<User[]> {
+    //https://jsonplaceholder.typicode.com/users
     return new Promise((resolve) => {
-      resolve(this.userData);
+      fetch('https://jsonplaceholder.typicode.com/users').then((response) => {
+        response.json().then((data) => {
+          resolve(data);
+        });
+      }
+      );
     });
   }
 }

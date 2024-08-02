d97945d98b8b85bb83941d48287ee680a122a599
LeventHAN <l.mehmed@faktion.com>
2024-08-02 19:59:33 +0200

    feat: finish p6

--- projects/06-input-output/src/app/listing/listing.component.ts
+++ projects/06-input-output/src/app/listing/listing.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Car } from '../car';
 
@@ -6,7 +6,46 @@ import { Car } from '../car';
   selector: 'app-listing',
   standalone: true,
   imports: [CommonModule],
-  template: ` <!-- listing markup goes here --> `,
+  template: ` <!-- listing markup goes here -->
+  <article class="listing">
+        <div class="image-parent">
+          <img class="product-image" src="https://placehold.co/100x100" />
+        </div>
+        <section class="details">
+          <p class="title">{{car.make+' '+car.model}}</p>
+          <hr />
+          <p class="detail">
+            <span>Year</span>
+            <span><!-- year -->{{car.year}}</span>
+          </p>
+          <div class="detail">
+            <span>Transmission</span>
+            <span><!-- transmission -->{{car.transmission}}</span>
+          </div>
+          <p class="detail">
+            <span>Mileage</span>
+            <span><!-- miles -->{{car.miles}}</span>
+          </p>
+          <p class="detail">
+            <span>Price</span>
+            <span><!-- price -->{{car.price}}</span>
+          </p>
+
+          <button class="save" (click)="saveCar()">Save</button>
+        </section>
+      </article>
+  `,
   styles: ``,
 })
-export class ListingComponent {}
+export class ListingComponent {
+  @Input({
+    required: true,
+  }) car!: Car;
+  @Output() handleCarSaved = new EventEmitter<Car>();
+  
+
+  saveCar() {
+    this.handleCarSaved.emit(this.car);
+  }
+
+}
